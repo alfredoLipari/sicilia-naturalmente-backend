8ba132f309e12b4b582174a8e08ee037e02b6969
Alfredo <alipari@deloitte.it>
2022-08-11 14:34:02 +0200

    test integration ci 3

--- src/main/java/it/elis/sicilianaturalmente/controller/LoginController.java
+++ src/main/java/it/elis/sicilianaturalmente/controller/LoginController.java
@@ -43,6 +43,7 @@ public class LoginController {
     public ResponseEntity<String> signin(@RequestParam("email") String email,@RequestParam("password") String password) {
         System.out.println(email);
         System.out.println(password);
+        System.out.println("lol");
         return ResponseEntity.ok(accountService.signin(email, password));
     }
 
