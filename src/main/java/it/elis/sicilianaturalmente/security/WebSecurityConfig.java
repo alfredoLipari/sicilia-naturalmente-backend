a1a4530387189803653ea081c9dc1520e52cc1a6
Alfredo <alipari@deloitte.it>
2022-08-12 15:29:56 +0200

    cors config

--- src/main/java/it/elis/sicilianaturalmente/security/WebSecurityConfig.java
+++ src/main/java/it/elis/sicilianaturalmente/security/WebSecurityConfig.java
@@ -91,7 +91,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private CorsConfiguration getCorsConfiguration(){
          CorsConfiguration corsConfiguration =new CorsConfiguration();
          corsConfiguration.setAllowCredentials(true);
-         corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+         corsConfiguration.setAllowedOriginPatterns(Arrays.asList("*"));
          corsConfiguration.setAllowedHeaders(Arrays.asList("Origin","Access-Control-Allow-Origin","Content-Type","Accept","Authorization","Origin, Accept","X-Requesed-With","Access-Control-Request-Method","Access-Control-Request-Headers"));
          corsConfiguration.setExposedHeaders(Arrays.asList("Origin","Content-Type","Accept","Authorization","Access-Control-Allow-Origin","Access-Control-Allow-Origin","Access-Control-Allow-Credentials"));
          corsConfiguration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
